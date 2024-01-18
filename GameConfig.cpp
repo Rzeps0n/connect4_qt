13617e745b88b939e06f69ac6790d383feafe0d0
rzeps0n <adam.rzepka3001@gmail.com>
2024-01-18 23:07:49 +0100

    changed colors to hex

--- GameConfig.cpp
+++ GameConfig.cpp
@@ -6,9 +6,9 @@ namespace GameConfig {
     int windowWidth = 800;
     int windowHeight = 600;
     bool allowWindowResize = false;
-    QString playerOneColor = "red";
-    QString playerTwoColor = "yellow";
-    QString tokenBackgroundColor = "darkgrey";
+    QString playerOneColor = "#FF0000";
+    QString playerTwoColor = "#FFFF00";
+    QString tokenBackgroundColor = "#C0C0C0";
     QString backgroundColor = "#383434";
     QString buttonColor = "#f0f0f0";
     QString buttonTextColor = "#000000";
